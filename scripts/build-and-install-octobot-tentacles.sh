c7518047932aaf5ff9868a46dee260f9371eea9a
Max <max@tfq.at>
2023-06-08 10:19:23 +0200

    fix typos in build scripts

--- scripts/build-and-install-octobot-tentacles.sh
+++ scripts/build-and-install-octobot-tentacles.sh
@@ -1,8 +1,8 @@
 # install tentacles to bump versions
 source .venv/bin/activate
 export PYTHONPATH=${PWD}/octobot-packages/Async-Channel:${PWD}/octobot-packages/OctoBot-Tentacles-Manager:${PWD}/octobot-packages/OctoBot-Commons:${PWD}/octobot-packages/OctoBot-Trading:${PWD}/octobot-packages/OctoBot-Backtesting:${PWD}/octobot-packages/OctoBot-evaluators:${PWD}/octobot-packages/OctoBot-Services:${PWD}/octobot-packages/trading-backend
-python /octobot-packages/OctoBot/start.py tentacles -p ./any_platform.zip -d ./octobot-packages/reference_tentacles
-python /octobot-packages/OctoBot/start.py tentacles --install --all --location ./output/any_platform.zip
+python octobot-packages/OctoBot/start.py tentacles -p ./any_platform.zip -d ./octobot-packages/reference_tentacles
+python octobot-packages/OctoBot/start.py tentacles --install --all --location ./output/any_platform.zip
 
 # clean
 rm -R ./output
