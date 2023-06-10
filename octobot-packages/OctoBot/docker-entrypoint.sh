548cfc04a55aa95f51e779d3311dbdd38cf6faf9
Max <max@tfq.at>
2023-06-10 13:28:09 +0200

    improve install scripts

--- octobot-packages/OctoBot/docker-entrypoint.sh
+++ octobot-packages/OctoBot/docker-entrypoint.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-# if [[ -n "${OCTOBOT_CONFIG}" ]]; then
-#   echo "$OCTOBOT_CONFIG" | tee /octobot/octobot/user/config.json > /dev/null
-# fi
-
 /octobot/Octane tentacles -p /octobot/output/any_platform.zip -d /octobot-packages/octobot-packages/reference_tentacles
 /octobot/Octane tentacles --install --all --location /octobot/output/any_platform.zip
 
