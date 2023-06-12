0c7a9cced500fcc9c77a80d8a24af11737203c86
Max <max@tfq.at>
2023-06-12 13:56:04 +0200

    fix linux docker install

--- octobot-packages/OctoBot/docker-entrypoint.sh
+++ octobot-packages/OctoBot/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-/octobot/Octane tentacles -p /octobot/output/any_platform.zip -d /octobot-packages/octobot-packages/reference_tentacles
+/octobot/Octane tentacles -p /octobot/output/any_platform.zip -d /octobot-packages/reference_tentacles
 /octobot/Octane tentacles --install --all --location /octobot/output/any_platform.zip
 
 /octobot/Octane
