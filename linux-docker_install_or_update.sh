26ffa0489519a2146382c5c91f743c77b85ff1e9
Max <max@tfq.at>
2023-06-17 10:24:29 +0200

    fix neural net training data

--- linux-docker_install_or_update.sh
+++ linux-docker_install_or_update.sh
@@ -11,4 +11,4 @@ docker build --tag octobot -f scripts/Dockerfile .
 docker stop octobot1
 docker rm octobot1
 docker run -d --name octobot1 -p 5001:5001 -p 9000:9000 -v $PWD/users/user1/user:/octobot/user -v $PWD/users/user1/tentacles:/octobot/tentacles -v $PWD/users/user1/logs:/octobot/logs -v $PWD/users/user1/backtesting:/octobot/backtesting octobot
-# docker logs -f octobot
+# docker logs -f octobot1
