e85aca0e888de3f80c83ffecd5f6cc1ef912eccc
Max <max@a42.ch>
2023-06-28 22:52:29 +0200

    fix docker entrypoint encoding

--- octobot-packages/OctoBot/docker-entrypoint.sh
+++ octobot-packages/OctoBot/docker-entrypoint.sh
@@ -1,4 +1,3 @@
 /octobot/Octane tentacles -p /octobot/output/any_platform.zip -d /octobot-packages/reference_tentacles
 /octobot/Octane tentacles --install --all --location /octobot/output/any_platform.zip
-
 /octobot/Octane
