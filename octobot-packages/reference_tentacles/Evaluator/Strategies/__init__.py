0ab6f887b2267643ad9df37f111f70836fc4b7ee
Max <max@tfq.at>
2023-07-01 14:49:34 +0200

    minor app store fix

--- octobot-packages/reference_tentacles/Evaluator/Strategies/__init__.py
+++ octobot-packages/reference_tentacles/Evaluator/Strategies/__init__.py
@@ -1,15 +1,6 @@
 from octobot_tentacles_manager.api.inspector import check_tentacle_version
 from octobot_commons.logging.logging_util import get_logger
 
-if check_tentacle_version('1.2.0', 'blank_strategy_evaluator', 'OctoBot-Default-Tentacles'):
-    try:
-        from .blank_strategy_evaluator import *
-    except Exception as e:
-        get_logger('TentacleLoader').error(f'Error when loading blank_strategy_evaluator: '
-                                           f'{e.__class__.__name__}{f" ({e})" if f"{e}" else ""}. If this '
-                                           f'error persists, try reinstalling your tentacles via '
-                                           f'"python start.py tentacles --install --all".')
-
 if check_tentacle_version('1.2.0', 'mixed_strategies_evaluator', 'OctoBot-Default-Tentacles'):
     try:
         from .mixed_strategies_evaluator import *
@@ -19,11 +10,20 @@ if check_tentacle_version('1.2.0', 'mixed_strategies_evaluator', 'OctoBot-Defaul
                                            f'error persists, try reinstalling your tentacles via '
                                            f'"python start.py tentacles --install --all".')
 
-if check_tentacle_version('1.2.0', 'dip_analyser_strategy_evaluator', 'OctoBot-Default-Tentacles'):
+if check_tentacle_version('1.2.0', 'blank_strategy_evaluator', 'OctoBot-Default-Tentacles'):
     try:
-        from .dip_analyser_strategy_evaluator import *
+        from .blank_strategy_evaluator import *
     except Exception as e:
-        get_logger('TentacleLoader').error(f'Error when loading dip_analyser_strategy_evaluator: '
+        get_logger('TentacleLoader').error(f'Error when loading blank_strategy_evaluator: '
+                                           f'{e.__class__.__name__}{f" ({e})" if f"{e}" else ""}. If this '
+                                           f'error persists, try reinstalling your tentacles via '
+                                           f'"python start.py tentacles --install --all".')
+
+if check_tentacle_version('1.4.0', 'time_frame_strategy_evaluator', 'Octane-Default-Tentacles'):
+    try:
+        from .time_frame_strategy_evaluator import *
+    except Exception as e:
+        get_logger('TentacleLoader').error(f'Error when loading time_frame_strategy_evaluator: '
                                            f'{e.__class__.__name__}{f" ({e})" if f"{e}" else ""}. If this '
                                            f'error persists, try reinstalling your tentacles via '
                                            f'"python start.py tentacles --install --all".')
@@ -37,16 +37,16 @@ if check_tentacle_version('1.2.0', 'move_signals_strategy_evaluator', 'OctoBot-D
                                            f'error persists, try reinstalling your tentacles via '
                                            f'"python start.py tentacles --install --all".')
 
-if check_tentacle_version('1.2.0', 'time_frame_strategy_evaluator', 'OctoBot-Default-Tentacles'):
+if check_tentacle_version('1.2.0', 'dip_analyser_strategy_evaluator', 'OctoBot-Default-Tentacles'):
     try:
-        from .time_frame_strategy_evaluator import *
+        from .dip_analyser_strategy_evaluator import *
     except Exception as e:
-        get_logger('TentacleLoader').error(f'Error when loading time_frame_strategy_evaluator: '
+        get_logger('TentacleLoader').error(f'Error when loading dip_analyser_strategy_evaluator: '
                                            f'{e.__class__.__name__}{f" ({e})" if f"{e}" else ""}. If this '
                                            f'error persists, try reinstalling your tentacles via '
                                            f'"python start.py tentacles --install --all".')
 
-if check_tentacle_version('1.2.0', 'real_time_strategy_evaluator', 'OctoBot-Default-Tentacles'):
+if check_tentacle_version('1.0.0', 'real_time_strategy_evaluator', 'Matrix-Pro-Tentacles'):
     try:
         from .real_time_strategy_evaluator import *
     except Exception as e:
