4f02cf929d0f0a0e6edd0482b6dc96083075373f
Max <max@a42.ch>
2023-07-02 00:36:56 +0200

    fix windows docker paths

--- windows-docker_install_or_update.ps1
+++ windows-docker_install_or_update.ps1
@@ -12,5 +12,5 @@ docker build --tag octane -f scripts/Dockerfile-win .
 
 docker stop octane1
 docker rm octane1
-docker run -d --name octane1 -p 5001:5001 -v $PSScriptRoot/users/user1/user:/octane/user -v $PSScriptRoot/users/user1/tentacles:/octane/tentacles -v $PSScriptRoot/users/user1/logs:/octane/logs -v $PSScriptRoot/users/user1/backtesting:/octane/backtesting octane
+docker run -d --name octane1 -p 5001:5001 -v $PSScriptRoot/users/user1/user:/octobot/user -v $PSScriptRoot/users/user1/tentacles:/octobot/tentacles -v $PSScriptRoot/users/user1/logs:/octobot/logs -v $PSScriptRoot/users/user1/backtesting:/octobot/backtesting octane
 docker logs -f octane1
