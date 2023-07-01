ec4784cc9195ef3633a739a74aca5288ccd6d06e
Max <max@a42.ch>
2023-07-01 23:09:51 +0200

    fix start after install

--- windows-install-repair-and-update-to-BETA-installation.ps1
+++ windows-install-repair-and-update-to-BETA-installation.ps1
@@ -4,4 +4,4 @@ git pull
 & $PSScriptRoot\scripts\repair_installation.ps1
 & $PSScriptRoot\scripts\windows-update-DEVELOPER-installation.ps1
 
-./windows-start-octobot.ps1
+./windows-start-octane.ps1
