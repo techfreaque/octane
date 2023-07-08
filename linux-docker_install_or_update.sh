be7b54d2a575d22a3c1ded185dc9960f836c0e95
Max <max@tfq.at>
2023-07-08 14:38:45 +0200

    allow multiple docker instances by env var

--- linux-docker_install_or_update.sh
+++ linux-docker_install_or_update.sh
@@ -8,7 +8,17 @@ chmod +x linux-docker_install_or_update.sh
 cp -n scripts/.env-example-unix .env
 docker build --tag octane -f scripts/Dockerfile .
 
-docker stop octane1
-docker rm octane1
-docker run -d --name octane1 -p 5001:5001 -p 9000:9000 -v $PWD/users/user1/user:/octobot/user -v $PWD/users/user1/tentacles:/octobot/tentacles -v $PWD/users/user1/logs:/octobot/logs -v $PWD/users/user1/backtesting:/octobot/backtesting octane
+source ./.env
+if [ -z "$AMOUNT_OF_DOCKER_INSTANCES" ]
+then
+      AMOUNT_OF_DOCKER_INSTANCES=1
+fi
+
+for n in $(seq 1 $AMOUNT_OF_DOCKER_INSTANCES)
+do
+    docker stop octane$n
+    docker rm octane$n
+    docker run -d --name octane$n -p 500$n:5001 -p 900$n:9000 -v $PWD/users/user$n/user:/octobot/user -v $PWD/users/user$n/tentacles:/octobot/tentacles -v $PWD/users/user$n/logs:/octobot/logs -v $PWD/users/user$n/backtesting:/octobot/backtesting octane
+done
+
 # docker logs -f octane1
